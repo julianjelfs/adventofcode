67b41672677cdaca8a7d00ed3174ee18674e0167
julianjelfs <julian.jelfs@gmail.com>
2015-12-04 20:44:43 +0000

    day 2

---
+++ test/adventofcode/core_test.clj
@@ -0,0 +1,11 @@
+(ns adventofcode.core-test
+  (:require [clojure.test :refer :all]
+            [adventofcode.core :refer :all]))
+
+(deftest test-total
+ (testing "Total"
+  (is (= 1586300 (total)))))
+  
+   
+    
+     
