1b91536a3b3bb331e976a139aa6e2eb3234d5f68
julianjelfs <julian.jelfs@gmail.com>
2015-12-05 15:39:09 +0000

    really irritating

--- test/adventofcode/day3_test.clj
+++ test/adventofcode/day3_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [adventofcode.day3 :refer :all]))
 
-(deftest parse-input-test
- (testing "parsing the input"
-  (is (= 8193 (count (parse-input))))))
-  
 (deftest test-houses-visited
  (testing "how many houses visited"
   (is (= 8193 (houses-visited)))))
