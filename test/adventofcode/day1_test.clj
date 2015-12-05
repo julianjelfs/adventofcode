b9502ede5fe90ce5334224684304a4298aacf83a
julianjelfs <julian.jelfs@gmail.com>
2015-12-05 15:55:45 +0000

    converted to day1 to use recur to avoid stack overflow

--- test/adventofcode/day1_test.clj
+++ test/adventofcode/day1_test.clj
@@ -2,11 +2,11 @@
   (:require [clojure.test :refer :all]
             [adventofcode.day1 :refer :all]))
 
-;(deftest test-what-floor
-; (testing "what floor?"
-;  (is (= 232 (what-floor?)))))
+(deftest test-what-floor
+ (testing "what floor?"
+  (is (= 232 (what-floor?)))))
   
-;(deftest test-what-index
-; (testing "what index"
-;  (is (= 1783 (what-index?)))))
+(deftest test-what-index
+ (testing "what index"
+  (is (= 1783 (what-index?)))))
 
